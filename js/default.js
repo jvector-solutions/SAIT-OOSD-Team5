7b92a61c589d3975ab4abca0c1f6f549076111e8
John Nguyen <jhatnguyen@gmail.com>
2014-11-05 16:00:05 -0700

    Index & CSS Changes

--- js/default.js
+++ js/default.js
@@ -1,3 +1,12 @@
+function toggle_visibility(id) {
+   var e = document.getElementById(id);
+   if(e.style.display == 'block') {
+      e.style.display = 'none';
+   } else {
+      e.style.display = 'block';
+   }
+    
+}
 
 function  formvalidation()
 {
