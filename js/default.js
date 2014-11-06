dac9027bab4465e571a63e7b94cb2222dc14e02d
John Nguyen <jhatnguyen@gmail.com>
2014-11-05 22:23:12 -0700

    More CSS Updates

--- js/default.js
+++ js/default.js
@@ -1,11 +1,10 @@
-function toggle_visibility(id) {
+function dropMenu(id) {
    var e = document.getElementById(id);
    if(e.style.display == 'block') {
       e.style.display = 'none';
    } else {
       e.style.display = 'block';
    }
-    
 }
 
 function  formvalidation()
